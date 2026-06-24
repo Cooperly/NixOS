f9452d59a32e69d4a3e0a6c7e5d9ed2b2bcfb5ec
void <void@freewifi.club>
2026-06-24 17:24:34 +0200

    wivrn stuff

--- config/modules/wivrn.nix
+++ config/modules/wivrn.nix
@@ -11,17 +11,19 @@
         extraServerFlags = [
           "--early-active-runtime"
         ];
-      };
-    
-    monado = {
-      enable = true;
-      defaultRuntime = true;
-    };
-  };
+        
+        monadoEnvironment = {
+          XRT_COMPOSITOR_USE_PRESENT_WAIT = "1";
+          U_PACING_COMP_TIME_FRACTION_PERCENT = "90";
 
-  systemd.user.services.monado.environment = {
-    XRT_COMPOSITOR_USE_PRESENT_WAIT = "1";
-    U_PACING_COMP_TIME_FRACTION_PERCENT = "90";
+          GBM_BACKEND = "nvidia-drm";
+          __GLX_VENDOR_LIBRARY_NAME = "nvidia";
+          LIBVA_DRIVER_NAME = "nvidia";
+
+          __GL_VRR_ALLOWED = "0";
+          __GL_GYSYNC_ALLOWED = "0";
+        };
+      };
   };
 
   environment.systemPackages = with pkgs; [
