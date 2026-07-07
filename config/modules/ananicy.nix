b6c7b0a998c98a88a736f8b56acc97beabb1fd0d
void <void@freewifi.club>
2026-07-07 12:19:22 +0200

    Update SCX and Ananicy

--- config/modules/ananicy.nix
+++ config/modules/ananicy.nix
@@ -2,5 +2,6 @@
   services.ananicy = {
     enable = true;
     package = pkgs.ananicy-cpp;
+    rulesProvider = pkgs.ananicy-rules-cachyos;
   };
 }
--- config/modules/scx.nix
+++ config/modules/scx.nix
@@ -1,9 +1,11 @@
-{ ... }: {
+{ pkgs-unstable, ... }: {
   services.scx = {
     enable = true;
     scheduler = "scx_lavd";
+    package = pkgs-unstable.scx.full;
     extraArgs = [
       "--performance"
+      "--pinned-slice-us 500"
       "--no-core-compaction"
     ];
   };
